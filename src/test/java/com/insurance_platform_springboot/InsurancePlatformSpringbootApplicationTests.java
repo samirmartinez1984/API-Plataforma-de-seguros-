95895f62546b4bb791b30967caec6a44e8144095
samir martinez acosta <samiresteyber0511@hotmail.com>
2026-08-04 22:25:13 -0500

    fix: activar perfil de test en contextLoads para usar H2 en CI

--- src/test/java/com/insurance_platform_springboot/InsurancePlatformSpringbootApplicationTests.java
+++ src/test/java/com/insurance_platform_springboot/InsurancePlatformSpringbootApplicationTests.java
@@ -2,8 +2,10 @@ package com.insurance_platform_springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InsurancePlatformSpringbootApplicationTests {
 
 	@Test
